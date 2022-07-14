ea0a51e21613ea0852bd27fa09882ac792a0cd06
MonsterBasket <blazza1@hotmail.com>
2022-07-14 15:14:25 +1000

    test complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const homeBase = 42;
+
+function distanceFromHqInBlocks(destination, start = homeBase){
+    let distance = destination - start;
+    return Math.abs(distance);
+}
+function distanceFromHqInFeet(destination){
+    return distanceFromHqInBlocks(destination) * 264;
+}
+function distanceTravelledInFeet(destination, start = homeBase){
+    return distanceFromHqInBlocks(destination, start) * 264;
+}
+function calculatesFarePrice(destination, start = homeBase){
+    let distance = distanceTravelledInFeet(destination, start);
+    if (distance <=400)
+        return 0;
+    else if (distance <=2000)
+        return (distance - 400) * 0.02;
+    else if (distance <=2500)
+        return 25;
+    else
+        return 'cannot travel that far';
+}
